74b58a8215e2439661119d9f698557ea07209128
Dave Lockhart <dave.lockhart@d2l.com>
2015-08-24 12:16:53 -0400

    removing non-implemented methods

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import {default as formatDateTime} from './date/format';
-import {default as formatInputDate} from './date/format-input';
-import {default as formatNumber} from './number/format';
-import {default as formatTime} from './time/format.js';
-import {default as parseDate} from './date/parse';
-import {default as parseNumber} from './number/parse';
-import {deafult as parseTime} from './time/parse';
+import {numberFormat} from './number/format';
 
 export default {
-	DateTime: {
-		formatDateTime: formatDateTime,
-		formatInputDate: formatInputDate,
-		formatTime: formatTime,
-		parseDate: parseDate,
-		parseTime: parseTime
-	},
-	Number: {
-		format: formatNumber,
-		parse: parseNumber
-	}
+	NumberFormat: numberFormat
 };
