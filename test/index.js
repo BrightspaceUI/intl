fc67d3adcc951d77508d44597739efbc1707c7b9
Dave Lockhart <dave.lockhart@d2l.com>
2017-12-01 11:10:07 -0500

    upgrading to latest NPM dependencies

--- test/index.js
+++ test/index.js
@@ -9,27 +9,27 @@ chai.should();
 describe('Intl', function() {
 
 	it('should be defined', function() {
-		expect(d2lIntl).to.be.defined;
+		expect(d2lIntl).to.not.be.undefined;
 	});
 
 	it('should expose DateTimeFormat class', function() {
-		expect(d2lIntl.DateTimeFormat).to.be.defined;
+		expect(d2lIntl.DateTimeFormat).to.not.be.undefined;
 	});
 
 	it('should expose DateTimeParse class', function() {
-		expect(d2lIntl.DateTimeParse).to.be.defined;
+		expect(d2lIntl.DateTimeParse).to.not.be.undefined;
 	});
 
 	it('should expose NumberFormat class', function() {
-		expect(d2lIntl.NumberFormat).to.be.defined;
+		expect(d2lIntl.NumberFormat).to.not.be.undefined;
 	});
 
 	it('should expose NumberParse class', function() {
-		expect(d2lIntl.NumberParse).to.be.defined;
+		expect(d2lIntl.NumberParse).to.not.be.undefined;
 	});
 
 	it('should expose LocaleProvider class', function() {
-		expect(d2lIntl.LocaleProvider).to.be.defined;
+		expect(d2lIntl.LocaleProvider).to.not.be.undefined;
 	});
 
 });
