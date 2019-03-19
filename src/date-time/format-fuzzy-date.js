4cc3cdcf9af4547eb4e3591a5a8f4e1fc6b03394
Chester Twomey <Chester.Twomey@D2L.com>
2019-03-19 18:21:28 -0500

    fix import, add comment about broken thing

--- src/date-time/format-fuzzy-date.js
+++ src/date-time/format-fuzzy-date.js
@@ -1,4 +1,4 @@
-import { DateTimeFormat } from './format';
+import DateTimeFormat from './format';
 import processPattern from '../util/process-pattern.js';
 
 const _msPerDay = 86400000;
@@ -27,6 +27,7 @@ FuzzyDateFormatter.prototype.getDateString = function(dateUTC)  {
 	} else if (calendarDateDiff >= 1 && calendarDateDiff < 3) {
 		feedbackDateString = this._getStringSomeDaysAgo(calendarDateDiff, feedbackDate);
 	} else if (sameYear) {
+		//these 2 functions give full date with time making the output wrong
 		var day = this._formatMonthDay(dateUTC);
 		var time = this._formatTime(dateUTC);
 		feedbackDateString = processPattern(this.format.dayAtTime, {'{day}': day, '{time}': time});
