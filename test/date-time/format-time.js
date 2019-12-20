8c32fe4e020389d4191d1b6a806907babf1133d0
Dave Lockhart <dave.lockhart@d2l.com>
2019-12-20 12:14:32 -0500

    new baseline time tests

--- test/date-time/format-time.js
+++ test/date-time/format-time.js
@@ -57,6 +57,44 @@ describe('DateTimeFormat', function() {
 			expect(value).to.equal('11:59 post meridiem');
 		});
 
+		[
+			{locale: 'ar-SA', expect: ['1:28 ص', '1:28 ص', '1:28 ص EST', '1:28 ص EST', '1:52 م', '1:52 م', '1:52 م EST', '1:52 م EST']},
+			{locale: 'da-DK', expect: ['1:28 AM', '1:28 AM', '1:28 AM EST', '1:28 AM EST', '1:52 PM', '1:52 PM', '1:52 PM EST', '1:52 PM EST']},
+			{locale: 'de-DE', expect: ['1:28 AM', '1:28 AM', '1:28 AM EST', '1:28 AM EST', '1:52 PM', '1:52 PM', '1:52 PM EST', '1:52 PM EST']},
+			{locale: 'en-CA', expect: ['1:28 AM', '1:28 AM', '1:28 AM EST', '1:28 AM EST', '1:52 PM', '1:52 PM', '1:52 PM EST', '1:52 PM EST']},
+			{locale: 'en-GB', expect: ['1:28 AM', '1:28 AM', '1:28 AM EST', '1:28 AM EST', '1:52 PM', '1:52 PM', '1:52 PM EST', '1:52 PM EST']},
+			{locale: 'en-US', expect: ['1:28 AM', '1:28 AM', '1:28 AM EST', '1:28 AM EST', '1:52 PM', '1:52 PM', '1:52 PM EST', '1:52 PM EST']},
+			{locale: 'es-MX', expect: ['01:28', '01:28', '01:28 EST', '01:28 EST', '13:52', '13:52', '13:52 EST', '13:52 EST']},
+			{locale: 'fr-FR', expect: ['01 h 28', '01 h 28', '01 h 28 EST', '01 h 28 EST', '13 h 52', '13 h 52', '13 h 52 EST', '13 h 52 EST']},
+			{locale: 'fr-CA', expect: ['01 h 28', '01 h 28', '01 h 28 EST', '01 h 28 EST', '13 h 52', '13 h 52', '13 h 52 EST', '13 h 52 EST']},
+			{locale: 'ja-JP', expect: ['1:28', '1:28', '1:28 EST', '1:28 EST', '13:52', '13:52', '13:52 EST', '13:52 EST']},
+			{locale: 'ko-KR', expect: ['오전 1:28', '오전 1:28', '오전 1:28 EST', '오전 1:28 EST', '오후 1:52', '오후 1:52', '오후 1:52 EST', '오후 1:52 EST']},
+			{locale: 'nl-NL', expect: ['1:28 AM', '1:28 AM', '1:28 AM EST', '1:28 AM EST', '1:52 PM', '1:52 PM', '1:52 PM EST', '1:52 PM EST']},
+			{locale: 'pt-BR', expect: ['1:28', '1:28', '1:28 EST', '1:28 EST', '13:52', '13:52', '13:52 EST', '13:52 EST']},
+			{locale: 'sv-SE', expect: ['01:28', '01:28', '01:28 EST', '01:28 EST', '13:52', '13:52', '13:52 EST', '13:52 EST']},
+			{locale: 'tr-TR', expect: ['01:28', '01:28', '01:28 EST', '01:28 EST', '13:52', '13:52', '13:52 EST', '13:52 EST']},
+			{locale: 'zh-CN', expect: ['1:28', '1:28', 'EST 1:28', 'EST 1:28', '13:52', '13:52', 'EST 13:52', 'EST 13:52']},
+			{locale: 'zh-TW', expect: ['上午 01:28', '上午 01:28', '上午 01:28 EST', '上午 01:28 EST', '下午 01:52', '下午 01:52', '下午 01:52 EST', '下午 01:52 EST']}
+		].forEach((input) => {
+			let index = -1;
+			['early', 'late'].forEach((timeOfDay) => {
+				['short', 'full'].forEach((format) => {
+					[true, false].forEach((hour24) => {
+						it(`should format ${input.locale}/${timeOfDay}/${format}/${hour24}`, () => {
+							index++;
+							const time = timeOfDay === 'early' ? new Date(2015, 7, 25, 1, 28) : new Date(2015, 7, 25, 13, 52);
+							const dtFormat = new DateTimeFormat(
+								input.locale,
+								{format: format, timezone: 'EST', locale: {date: {hour24: hour24}}}
+							);
+							const value = dtFormat.formatTime(time);
+							expect(value).to.equal(input.expect[index]);
+						});
+					});
+				});
+			});
+		});
+
 	});
 
 });
