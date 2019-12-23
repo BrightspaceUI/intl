ffe24d0cdaa0b4566c59ea6b70183590e8c84937
Dave Lockhart <dave.lockhart@d2l.com>
2019-12-23 15:38:24 -0500

    removing v2 files

--- src/file-size/format.js
+++
@@ -1,9 +0,0 @@
-import formatFileSize from './format-file-size.js';
-import localeProvider from '../locale-provider.js';
-
-export default function FileSizeFormat(locale) {
-	this.localeData = localeProvider(locale);
-}
-FileSizeFormat.prototype.format = function(fileSize) {
-	return formatFileSize(fileSize, this.localeData);
-};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-import dateTimeFormat from './date-time/format.js';
-import dateTimeParse from './date-time/parse.js';
-import numberFormat from './number/format.js';
-import numberParse from './number/parse.js';
-import fileSizeFormat from './file-size/format.js';
-import localeProvider from './locale-provider.js';
-
-export default {
-	DateTimeFormat: dateTimeFormat,
-	DateTimeParse: dateTimeParse,
-	NumberFormat: numberFormat,
-	NumberParse: numberParse,
-	FileSizeFormat: fileSizeFormat,
-	LocaleProvider: localeProvider
-};
+export {formatDate, formatTime, parseDate, parseTime} from './dateTime.js';
+export {formatFileSize} from './fileSize.js';
+export {formatNumber, formatPercent, parseNumber} from './number.js';
