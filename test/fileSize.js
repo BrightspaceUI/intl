190f45b066e3465238b22d5d5eb9cfd308dc2c3f
Dave Lockhart <dave.lockhart@d2l.com>
2019-12-23 10:51:49 -0500

    simplifying file size formatting tests

--- test/fileSize.js
+++ test/fileSize.js
@@ -27,103 +27,39 @@ describe('formatFileSize', () => {
 		{value: -1073741824, expect: '-1 GB'},
 		{value: 1881111114, expect: '1.75 GB'}
 	].forEach((input) => {
-		it(`should correct parse the file-size "${input.value}"`, () => {
+		it(`should format "${input.value}"`, () => {
 			const value = formatFileSize(input.value);
 			expect(value).to.equal(input.expect);
 		});
 	});
 
 	[
-		{byteNum:1, expect:'1 بايت', locale:'ar-SA'},
-		{byteNum:2, expect:'2 بايت', locale:'ar-SA'},
-		{byteNum:1024, expect:'1 كيلو بايت', locale:'ar-SA'},
-		{byteNum:1048576, expect:'1 ميغا بايت', locale:'ar-SA'},
-		{byteNum:1073741824, expect:'1 غيغا بايت', locale:'ar-SA'},
-		{byteNum:1, expect:'1 byte', locale:'da-DK'},
-		{byteNum:2, expect:'2 bytes', locale:'da-DK'},
-		{byteNum:1024, expect:'1 KB', locale:'da-DK'},
-		{byteNum:1048576, expect:'1 MB', locale:'da-DK'},
-		{byteNum:1073741824, expect:'1 GB', locale:'da-DK'},
-		{byteNum:1, expect:'1 byte', locale:'de-DE'},
-		{byteNum:2, expect:'2 bytes', locale:'de-DE'},
-		{byteNum:1024, expect:'1 KB', locale:'de-DE'},
-		{byteNum:1048576, expect:'1 MB', locale:'de-DE'},
-		{byteNum:1073741824, expect:'1 GB', locale:'de-DE'},
-		{byteNum:1, expect:'1 byte', locale:'en-CA'},
-		{byteNum:2, expect:'2 bytes', locale:'en-CA'},
-		{byteNum:1024, expect:'1 KB', locale:'en-CA'},
-		{byteNum:1048576, expect:'1 MB', locale:'en-CA'},
-		{byteNum:1073741824, expect:'1 GB', locale:'en-CA'},
-		{byteNum:1, expect:'1 byte', locale:'en-GB'},
-		{byteNum:2, expect:'2 bytes', locale:'en-GB'},
-		{byteNum:1024, expect:'1 KB', locale:'en-GB'},
-		{byteNum:1048576, expect:'1 MB', locale:'en-GB'},
-		{byteNum:1073741824, expect:'1 GB', locale:'en-GB'},
-		{byteNum:1, expect:'1 byte', locale:'en-US'},
-		{byteNum:2, expect:'2 bytes', locale:'en-US'},
-		{byteNum:1024, expect:'1 KB', locale:'en-US'},
-		{byteNum:1048576, expect:'1 MB', locale:'en-US'},
-		{byteNum:1073741824, expect:'1 GB', locale:'en-US'},
-		{byteNum:1, expect:'1 byte', locale:'es-MX'},
-		{byteNum:2, expect:'2 bytes', locale:'es-MX'},
-		{byteNum:1024, expect:'1 KB', locale:'es-MX'},
-		{byteNum:1048576, expect:'1 MB', locale:'es-MX'},
-		{byteNum:1073741824, expect:'1 GB', locale:'es-MX'},
-		{byteNum:1, expect:'1 octet', locale:'fr-CA'},
-		{byteNum:2, expect:'2 octets', locale:'fr-CA'},
-		{byteNum:1024, expect:'1 Ko', locale:'fr-CA'},
-		{byteNum:1048576, expect:'1 Mo', locale:'fr-CA'},
-		{byteNum:1073741824, expect:'1 Go', locale:'fr-CA'},
-		{byteNum:1, expect:'1 octet', locale:'fr-FR'},
-		{byteNum:2, expect:'2 octets', locale:'fr-FR'},
-		{byteNum:1024, expect:'1 Ko', locale:'fr-FR'},
-		{byteNum:1048576, expect:'1 Mo', locale:'fr-FR'},
-		{byteNum:1073741824, expect:'1 Go', locale:'fr-FR'},
-		{byteNum:1, expect:'1 バイト', locale:'ja-JP'},
-		{byteNum:2, expect:'2 バイト', locale:'ja-JP'},
-		{byteNum:1024, expect:'1 KB', locale:'ja-JP'},
-		{byteNum:1048576, expect:'1 MB', locale:'ja-JP'},
-		{byteNum:1073741824, expect:'1 GB', locale:'ja-JP'},
-		{byteNum:1, expect:'1 바이트', locale:'ko-KR'},
-		{byteNum:2, expect:'2 바이트', locale:'ko-KR'},
-		{byteNum:1024, expect:'1 KB', locale:'ko-KR'},
-		{byteNum:1048576, expect:'1 MB', locale:'ko-KR'},
-		{byteNum:1073741824, expect:'1 GB', locale:'ko-KR'},
-		{byteNum:1, expect:'1 byte', locale:'nl-NL'},
-		{byteNum:2, expect:'2 bytes', locale:'nl-NL'},
-		{byteNum:1024, expect:'1 KB', locale:'nl-NL'},
-		{byteNum:1048576, expect:'1 MB', locale:'nl-NL'},
-		{byteNum:1073741824, expect:'1 GB', locale:'nl-NL'},
-		{byteNum:1, expect:'1 byte', locale:'pt-BR'},
-		{byteNum:2, expect:'2 bytes', locale:'pt-BR'},
-		{byteNum:1024, expect:'1 KB', locale:'pt-BR'},
-		{byteNum:1048576, expect:'1 MB', locale:'pt-BR'},
-		{byteNum:1073741824, expect:'1 GB', locale:'pt-BR'},
-		{byteNum:1, expect:'1 byte', locale:'sv-SE'},
-		{byteNum:2, expect:'2 byte', locale:'sv-SE'},
-		{byteNum:1024, expect:'1 KB', locale:'sv-SE'},
-		{byteNum:1048576, expect:'1 MB', locale:'sv-SE'},
-		{byteNum:1073741824, expect:'1 GB', locale:'sv-SE'},
-		{byteNum:1, expect:'1 bayt', locale:'tr-TR'},
-		{byteNum:2, expect:'2 bayt', locale:'tr-TR'},
-		{byteNum:1024, expect:'1 KB', locale:'tr-TR'},
-		{byteNum:1048576, expect:'1 MB', locale:'tr-TR'},
-		{byteNum:1073741824, expect:'1 GB', locale:'tr-TR'},
-		{byteNum:1, expect:'1 字节', locale:'zh-CN'},
-		{byteNum:2, expect:'2 字节', locale:'zh-CN'},
-		{byteNum:1024, expect:'1 KB', locale:'zh-CN'},
-		{byteNum:1048576, expect:'1 MB', locale:'zh-CN'},
-		{byteNum:1073741824, expect:'1 GB', locale:'zh-CN'},
-		{byteNum:1, expect:'1 位元組', locale:'zh-TW'},
-		{byteNum:2, expect:'2 位元組', locale:'zh-TW'},
-		{byteNum:1024, expect:'1 KB', locale:'zh-TW'},
-		{byteNum:1048576, expect:'1 MB', locale:'zh-TW'},
-		{byteNum:1073741824, expect:'1 GB', locale:'zh-TW'}
+		{locale: 'ar-SA', expect: ['1 بايت', '2 بايت', '1 كيلو بايت', '1 ميغا بايت', '1 غيغا بايت']},
+		{locale: 'da-DK', expect: ['1 byte', '2 bytes', '1 KB', '1 MB', '1 GB']},
+		{locale: 'de-DE', expect: ['1 byte', '2 bytes', '1 KB', '1 MB', '1 GB']},
+		{locale: 'en-CA', expect: ['1 byte', '2 bytes', '1 KB', '1 MB', '1 GB']},
+		{locale: 'en-GB', expect: ['1 byte', '2 bytes', '1 KB', '1 MB', '1 GB']},
+		{locale: 'en-US', expect: ['1 byte', '2 bytes', '1 KB', '1 MB', '1 GB']},
+		{locale: 'es-MX', expect: ['1 byte', '2 bytes', '1 KB', '1 MB', '1 GB']},
+		{locale: 'fr-CA', expect: ['1 octet', '2 octets', '1 Ko', '1 Mo', '1 Go']},
+		{locale: 'fr-FR', expect: ['1 octet', '2 octets', '1 Ko', '1 Mo', '1 Go']},
+		{locale: 'ja-JP', expect: ['1 バイト', '2 バイト', '1 KB', '1 MB', '1 GB']},
+		{locale: 'ko-KR', expect: ['1 바이트', '2 바이트', '1 KB', '1 MB', '1 GB']},
+		{locale: 'nl-NL', expect: ['1 byte', '2 bytes', '1 KB', '1 MB', '1 GB']},
+		{locale: 'pt-BR', expect: ['1 byte', '2 bytes', '1 KB', '1 MB', '1 GB']},
+		{locale: 'sv-SE', expect: ['1 byte', '2 byte', '1 KB', '1 MB', '1 GB']},
+		{locale: 'tr-TR', expect: ['1 bayt', '2 bayt', '1 KB', '1 MB', '1 GB']},
+		{locale: 'zh-CN', expect: ['1 字节', '2 字节', '1 KB', '1 MB', '1 GB']},
+		{locale: 'zh-TW', expect: ['1 位元組', '2 位元組', '1 KB', '1 MB', '1 GB']},
 	].forEach((input) => {
-		it(`should parse the file-size: "${input.byteNum}" bytes for locale "${input.locale}"`, () => {
-			setDocumentLanguage(input.locale);
-			const value = formatFileSize(input.byteNum);
-			expect(value).to.equal(input.expect);
+		let index = -1;
+		[1, 2, 1024, 1048576, 1073741824].forEach((size) => {
+			it(`should format "${size}" bytes for locale "${input.locale}"`, () => {
+				index++;
+				setDocumentLanguage(input.locale);
+				const value = formatFileSize(size);
+				expect(value).to.equal(input.expect[index]);
+			});
 		});
 	});
 
