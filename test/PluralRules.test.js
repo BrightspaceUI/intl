78efc888c2d6945840f7f10ee140bea716c441c3
Danny Gleckler <daniel.gleckler@d2l.com>
2024-09-24 16:40:07 -0400

    Add more tests; Fix supportedLocalesOf and getCanonicalLocales

--- test/PluralRules.test.js
+++ test/PluralRules.test.js
@@ -1,6 +1,6 @@
+import '../lib/PluralRules.js';
 import { expect } from '@brightspace-ui/testing';
 import { getDocumentLocaleSettings } from '../lib/common.js';
-import { getSeparator } from '../lib/PluralRules.js';
 
 describe('PluralRules', () => {
 
@@ -8,14 +8,70 @@ describe('PluralRules', () => {
 
 	afterEach(() => documentLocaleSettings.reset());
 
+	it('extends native Intl.PluralRules', () => {
+		const native = Object.getPrototypeOf(Intl.PluralRules);
+		expect(Intl.PluralRules.shim).to.be.true;
+		expect(native).to.have.property('name', 'PluralRules');
+		expect(native).to.not.have.property('shim');
+	});
+
+	it('uses native data by default', () => {
+		const shim = new Intl.PluralRules('cy');
+		const native = new (Object.getPrototypeOf(Intl.PluralRules))('cy');
+		expect(shim.resolvedOptions()).to.deep.equal(native.resolvedOptions());
+	});
+
+	it('resolves to canonical locales', () => {
+		expect(new Intl.PluralRules('mao').resolvedOptions().locale).to.equal('mi');
+		expect(new Intl.PluralRules('mri').resolvedOptions().locale).to.equal('mi');
+		expect(new Intl.PluralRules(['abcdefg', 'mri']).resolvedOptions().locale).to.equal('mi');
+	});
+
+	it('includes custom locales as supported', () => {
+		expect(Intl.PluralRules.supportedLocalesOf(['abc', 'mao', 'en'])).to.deep.equal(['mi', 'en']);
+	});
+
 	[
-		{ locale: 'mi', type: 'cardinal', expect: { categories: [ 'one', 'other' ] } },
-		{ locale: 'mi', type: 'ordinal', expect: { categories: [ 'other' ] } }
-	].forEach(({ locale, type, expect }) => {
-		it(`should use custom data for "${locale}"`, () => {
-			documentLocaleSettings.language = locale;
-			const pluralRules = new Intl.PluralRules(locale, { type });
-			expect(pluralRules).to.have.deeo.properties(expect);
+		{
+			locale: 'mi',
+			type: 'cardinal',
+			options: {
+				locale: 'mi',
+				shim: true,
+				pluralCategories: [ 'one', 'other' ]
+			},
+			select: {
+				one: [1],
+				other: [0, 2, 3, 11]
+			}
+		},
+		{
+			locale: 'mi',
+			type: 'ordinal',
+			options: {
+				locale: 'mi',
+				shim: true,
+				pluralCategories: [ 'other' ]
+			},
+			select: {
+				other: [0, 1, 2, 3, 11]
+			}
+		}
+	].forEach(({ locale, type, options, select }) => {
+
+		documentLocaleSettings.language = locale;
+		const pluralRules = new Intl.PluralRules(locale, { type });
+
+		it(`should use custom ${type} data for "${locale}"`, () => {
+			expect(pluralRules.resolvedOptions()).to.deep.include(options);
+		});
+
+		it(`should select the correct ${type} number categories for "${locale}"`, () => {
+			options.pluralCategories.forEach(cat => {
+				select[cat].forEach(num => {
+					expect(pluralRules.select(num)).to.equal(cat);
+				});
+			});
 		});
 	});
 
