c9b58884b66e1d93c3477d3be4987f671c074c90
GZolla <43836485+GZolla@users.noreply.github.com>
2024-11-25 10:41:48 -0800

    Use new shared configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,12 @@
-import { fileURLToPath } from 'node:url';
-import { FlatCompat } from '@eslint/eslintrc';
-import { includeIgnoreFile } from '@eslint/compat';
-import js from '@eslint/js';
-import path from 'node:path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
-});
-const gitignorePath = path.resolve(__dirname, '.gitignore');
+import { addExtensions, browserConfig, setDirectoryConfigs, testingConfig } from 'eslint-config-brightspace';
 
 export default [
-	includeIgnoreFile(gitignorePath),
-	...compat.extends('brightspace/browser-config').map(config => ({
-		...config,
-		files: ['**/*.js'],
-	})),
-	...compat.extends('brightspace/testing-config').map(config => ({
-		...config,
-		files: ['test/*'],
-	})),
+	...addExtensions(setDirectoryConfigs(
+		browserConfig,
+		{
+			'test': testingConfig
+		}
+	), ['js']),
 	{
 		files: ['helpers/getLocalizeResources.js'],
 
