122428445ca154c451f5ee90a7a792aca0b0113e
tie.d2l.dev <TieUser@d2l.com>
2025-02-24 20:47:53 +0000

    Translation Updates

--- lang/haw.js
+++ lang/haw.js
@@ -2,14 +2,14 @@ export default {
 	"intl-common:characters:apostrophe": "apostrophe", // short name or description of the "'" character
 	"intl-common:characters:ampersand": "ampersand", // short name or description of the "&" character
 	"intl-common:characters:asterisk": "asterisk", // short name or description of the "*" character
-	"intl-common:characters:backslash": "backslash", // short name or description of the "\" character
-	"intl-common:characters:colon": "colon", // short name or description of the ":" character
-	"intl-common:characters:comma": "comma", // short name or description of the "," character
-	"intl-common:characters:greaterThan": "greater-than sign", // short name or description of the ">" character
-	"intl-common:characters:lessThan": "less-than sign", // short name or description of the "<" character
-	"intl-common:characters:numberSign": "number sign", // short name or description of the "#" character
-	"intl-common:characters:percentSign": "percent sign", // short name or description of the "%" character
-	"intl-common:characters:pipe": "pipe", // short name or description of the "|" character
-	"intl-common:characters:questionMark": "question mark", // short name or description of the "?" character
-	"intl-common:characters:quotationMark": "quotation mark", // short name or description of the '"' character
+	"intl-common:characters:backslash": "kaha kua hope", // short name or description of the "\" character
+	"intl-common:characters:colon": "kolonā", // short name or description of the ":" character
+	"intl-common:characters:comma": "koma", // short name or description of the "," character
+	"intl-common:characters:greaterThan": "nui-i ka hoailona", // short name or description of the ">" character
+	"intl-common:characters:lessThan": "hōʻailona liʻiliʻi", // short name or description of the "<" character
+	"intl-common:characters:numberSign": "hōʻailona helu", // short name or description of the "#" character
+	"intl-common:characters:percentSign": "hōʻailona pakeneka", // short name or description of the "%" character
+	"intl-common:characters:pipe": "paipu", // short name or description of the "|" character
+	"intl-common:characters:questionMark": "kaha ninau", // short name or description of the "?" character
+	"intl-common:characters:quotationMark": "kaha puana'i", // short name or description of the '"' character
 };
