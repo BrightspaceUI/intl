cec8ce572a4a9751fc0c9f932c796608017695fe
Dave Lockhart <dlockhart@users.noreply.github.com>
2025-05-26 14:45:52 -0400

    fix: backport translations to June release (#253)

---
+++ lang/mi.js
@@ -0,0 +1,15 @@
+export default {
+	"intl-common:characters:apostrophe": "tohutō", // short name or description of the "'" character
+	"intl-common:characters:ampersand": "me", // short name or description of the "&" character
+	"intl-common:characters:asterisk": "whetu-riki", // short name or description of the "*" character
+	"intl-common:characters:backslash": "Rarapa whakahoki", // short name or description of the "\" character
+	"intl-common:characters:colon": "kopirua", // short name or description of the ":" character
+	"intl-common:characters:comma": "korowhitiwhiti", // short name or description of the "," character
+	"intl-common:characters:greaterThan": "tohu nui-ake", // short name or description of the ">" character
+	"intl-common:characters:lessThan": "tohu iti iho", // short name or description of the "<" character
+	"intl-common:characters:numberSign": "tohu tau", // short name or description of the "#" character
+	"intl-common:characters:percentSign": "tohu o te ōrau", // short name or description of the "%" character
+	"intl-common:characters:pipe": "paipa", // short name or description of the "|" character
+	"intl-common:characters:questionMark": "tohu pātai", // short name or description of the "?" character
+	"intl-common:characters:quotationMark": "tohu whakatikatika", // short name or description of the '"' character
+};
