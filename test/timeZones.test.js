c389dca85b9cf4d29beca3c8788fc62158ecbe28
Danny Gleckler <daniel.gleckler@d2l.com>
2025-08-27 14:22:16 -0400

    basic timezone support

---
+++ test/timeZones.test.js
@@ -0,0 +1,30 @@
+import {
+	getTimeZonesData,
+	timeZoneIdentifiers,
+	validateTimeZone
+} from '../lib/timeZones.js';
+import { expect } from '@brightspace-ui/testing';
+import { getDocumentLocaleSettings } from '../lib/common.js';
+
+describe('timeZones', () => {
+
+	const documentLocaleSettings = getDocumentLocaleSettings();
+
+	afterEach(() => documentLocaleSettings.reset());
+
+	describe('getTimeZones', () => {
+
+		it('should return all time zones by default', () => {
+			const timeZones = getTimeZonesData();
+			expect(timeZones.length).to.equal(timeZoneIdentifiers.length);
+			expect(timeZones[0]).to.have.all.keys('abbreviation', 'city', 'friendlyName', 'localName', 'offset');
+		});
+
+		it('should respect region parameter', () => {
+			const timeZones = getTimeZonesData('US');
+			expect(timeZones.length).to.equal(new Intl.Locale('ar-US').getTimeZones().length)
+			expect(timeZones[0]).to.have.all.keys('abbreviation', 'city', 'friendlyName', 'localName', 'offset');
+		})
+
+	});
+});
