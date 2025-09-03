5f5896d5b808c9eb7757a841eabdcd524decab5c
tie.d2l.dev <TieUser@d2l.com>
2025-09-03 15:17:40 +0000

    Translation Updates

---
+++ lang/vi.js
@@ -0,0 +1,18 @@
+export default {
+	"intl-common:characters:apostrophe": "dấu nháy đơn", // short name or description of the "'" character
+	"intl-common:characters:ampersand": "dấu và", // short name or description of the "&" character
+	"intl-common:characters:asterisk": "dấu hoa thị", // short name or description of the "*" character
+	"intl-common:characters:backslash": "dấu gạch chéo ngược", // short name or description of the "\" character
+	"intl-common:characters:colon": "dấu hai chấm", // short name or description of the ":" character
+	"intl-common:characters:comma": "dấu phẩy", // short name or description of the "," character
+	"intl-common:characters:greaterThan": "dấu lớn hơn", // short name or description of the ">" character
+	"intl-common:characters:lessThan": "dấu bé hơn", // short name or description of the "<" character
+	"intl-common:characters:numberSign": "dấu thăng", // short name or description of the "#" character
+	"intl-common:characters:percentSign": "dấu phần trăm", // short name or description of the "%" character
+	"intl-common:characters:pipe": "dấu gạch đứng", // short name or description of the "|" character
+	"intl-common:characters:questionMark": "dấu chấm hỏi", // short name or description of the "?" character
+	"intl-common:characters:quotationMark": "dấu nháy kép", // short name or description of the '"' character
+	"intl-common:navigation:back:title": "Back", // term for navigating back to the previous page (in Title Case)
+	"intl-common:navigation:next:title": "Next", // term for navigating to the next page or item (in Title Case)
+	"intl-common:navigation:previous:title": "Trước", // term for navigating to the previous page or item (in Title Case)
+};
