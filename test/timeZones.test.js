a85050e77c2fcccbf28f40679abeebf423f47353
Danny Gleckler <daniel.gleckler@gmail.com>
2025-09-03 14:40:56 -0400

    feat: Add timezone data and tools (#273)
    * basic timezone support
    
    * Add data and mapper
    
    * Fix lint
    
    * Reorganize + tests
    
    * Add country and inputName
    
    * Update lib/timeZones.js
    
    Co-authored-by: GZolla <43836485+GZolla@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: GZolla <43836485+GZolla@users.noreply.github.com>

---
+++ test/timeZones.test.js
@@ -0,0 +1,75 @@
+import {
+	getTimeZoneData,
+	getTimeZonesData,
+	timeZoneIdentifiers,
+	validateTimeZone
+} from '../lib/timeZones.js';
+import { expect } from '@brightspace-ui/testing';
+import { getDocumentLocaleSettings } from '../lib/common.js';
+
+describe('timeZones', () => {
+
+	const documentLocaleSettings = getDocumentLocaleSettings();
+
+	document.documentElement.setAttribute('lang', 'en-US');
+
+	afterEach(() => documentLocaleSettings.reset());
+
+	describe('getTimeZonesData', () => {
+
+		it('should return all time zones by default', async() => {
+			const timeZones = await getTimeZonesData();
+			expect(timeZones.length).to.equal(timeZoneIdentifiers.length);
+			expect(timeZones[0]).to.have.all.keys('abbreviation', 'city', 'country', 'friendlyName', 'identifier', 'inputName', 'localName', 'offset');
+		});
+
+		it('should respect region parameter', async() => {
+			const timeZones = await getTimeZonesData('US');
+			expect(timeZones.length).to.equal(new Intl.Locale('ar-US').getTimeZones?.().length || 29);
+			expect(timeZones[0]).to.have.all.keys('abbreviation', 'city', 'country', 'friendlyName', 'identifier', 'inputName', 'localName', 'offset');
+		});
+
+		it('should respect modules parameter', async() => {
+			const expects = navigator.userAgent.includes('Firefox') ? [0, 2] : [29, 29];
+			let timeZones = await getTimeZonesData('US', 'africa');
+			expect(timeZones.length).to.equal(expects[0]);
+			timeZones = await getTimeZonesData('US', '_test');
+			expect(timeZones.length).to.equal(expects[1]);
+		});
+
+	});
+
+	describe('getTimeZoneData', () => {
+		it('should return valid time zone data', async() => {
+			const timeZoneId = 'America/Los_Angeles';
+
+			const timeZones = await getTimeZoneData(timeZoneId);
+			expect(timeZones).to.deep.equal({
+				abbreviation: 'PDT',
+				city: 'Los Angeles',
+				country: 'United States',
+				friendlyName: 'United States - Los Angeles',
+				identifier: 'America/Los_Angeles',
+				localName: 'Pacific Daylight Time',
+				offset: 'GMT-07:00',
+				inputName: '(GMT-07:00) Los Angeles - Pacific Daylight Time'
+			});
+		});
+	});
+
+	describe('validateTimeZone', () => {
+
+		it('should validate known time zones', () => {
+			expect(validateTimeZone('America/New_York')).to.be.true;
+			expect(validateTimeZone('Europe/London')).to.be.true;
+			expect(validateTimeZone('Asia/Kolkata')).to.be.true;
+		});
+
+		it('should invalidate unknown time zones', () => {
+			expect(validateTimeZone('America/Not_A_Time_Zone')).to.be.false;
+			expect(validateTimeZone('Not_A_Region/Not_A_City')).to.be.false;
+			expect(validateTimeZone('Invalid/Invalid')).to.be.false;
+		});
+
+	});
+});
