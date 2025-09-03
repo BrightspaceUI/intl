26aa8c51d2026c752b162728daaec5d9af6a92f4
tie.d2l.dev <TieUser@d2l.com>
2025-09-03 15:48:05 +0000

    Translation Updates

---
+++ lang/th.js
@@ -0,0 +1,18 @@
+export default {
+	"intl-common:characters:apostrophe": "'", // short name or description of the "'" character
+	"intl-common:characters:ampersand": "&", // short name or description of the "&" character
+	"intl-common:characters:asterisk": "*", // short name or description of the "*" character
+	"intl-common:characters:backslash": "\\", // short name or description of the "\" character
+	"intl-common:characters:colon": ":", // short name or description of the ":" character
+	"intl-common:characters:comma": ",", // short name or description of the "," character
+	"intl-common:characters:greaterThan": ">", // short name or description of the ">" character
+	"intl-common:characters:lessThan": "<", // short name or description of the "<" character
+	"intl-common:characters:numberSign": "#", // short name or description of the "#" character
+	"intl-common:characters:percentSign": "%", // short name or description of the "%" character
+	"intl-common:characters:pipe": "|", // short name or description of the "|" character
+	"intl-common:characters:questionMark": "?", // short name or description of the "?" character
+	"intl-common:characters:quotationMark": "\"-\"", // short name or description of the '"' character
+	"intl-common:navigation:back:title": "Back", // term for navigating back to the previous page (in Title Case)
+	"intl-common:navigation:next:title": "ถัดไป", // term for navigating to the next page or item (in Title Case)
+	"intl-common:navigation:previous:title": "ก่อนหน้า", // term for navigating to the previous page or item (in Title Case)
+};
