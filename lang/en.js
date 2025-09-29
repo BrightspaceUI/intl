5bb4c4c10e2b567211dc19d3beda5b9d3107731a
Dave Lockhart <dlockhart@users.noreply.github.com>
2025-09-29 16:40:29 -0400

    move continue into navigation

--- lang/en.js
+++ lang/en.js
@@ -6,7 +6,6 @@ export default {
 	"intl-common:actions:clear": "Clear", // clear action (in Title Case)
 	"intl-common:actions:close": "Close", // close action (in Title Case)
 	"intl-common:actions:confirm": "Confirm", // confirm action (in Title Case)
-	"intl-common:actions:continue": "Continue", // continue action (in Title Case)
 	"intl-common:actions:copy": "Copy", // copy action (in Title Case)
 	"intl-common:actions:create": "Create", // create action (in Title Case)
 	"intl-common:actions:delete": "Delete", // delete action (in Title Case)
@@ -38,6 +37,7 @@ export default {
 	"intl-common:confirm:ok": "OK", // okay response to a question
 	"intl-common:confirm:yes": "Yes", // yes response to a question (in Title Case)
 	"intl-common:navigation:back:title": "Back", // term for navigating back to the previous page (in Title Case)
+	"intl-common:navigation:continue:title": "Continue", // continue action (in Title Case)
 	"intl-common:navigation:next:title": "Next", // term for navigating to the next page or item (in Title Case)
 	"intl-common:navigation:previous:title": "Previous", // term for navigating to the previous page or item (in Title Case)
 };
