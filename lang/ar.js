d076a6310c0c544f304f2fc0f2bf1ee24b165535
tie.d2l.dev <TieUser@d2l.com>
2025-10-16 13:15:21 +0000

    Translation Updates

--- lang/ar.js
+++ lang/ar.js
@@ -17,10 +17,10 @@ export default {
 	"intl-common:actions:save": "حفظ", // save action (in Title Case)
 	"intl-common:actions:saveAndClose": "حفظ وإغلاق", // save and close action (in Title Case)
 	"intl-common:actions:saveAndNew": "حفظ وجديد", // save and new action (in Title Case)
-	"intl-common:actions:search": "بحث", // search action (in Title Case)
-	"intl-common:actions:submit": "إرسال", // submit action (in Title Case)
+	"intl-common:actions:search": "البحث", // search action (in Title Case)
+	"intl-common:actions:submit": "تسليم", // submit action (in Title Case)
 	"intl-common:actions:update": "تحديث", // update action (in Title Case)
-	"intl-common:characters:ampersand": "علامة العطف ”&“", // short name or description of the "&" character
+	"intl-common:characters:ampersand": "علامة العطف \"&\"", // short name or description of the "&" character
 	"intl-common:characters:apostrophe": "الفاصلة العليا", // short name or description of the "'" character
 	"intl-common:characters:asterisk": "العلامة النجمية", // short name or description of the "*" character
 	"intl-common:characters:backslash": "الشرطة المائلة", // short name or description of the "\" character
@@ -34,9 +34,9 @@ export default {
 	"intl-common:characters:questionMark": "علامة الاستفهام", // short name or description of the "?" character
 	"intl-common:characters:quotationMark": "علامة الاقتباس", // short name or description of the '"' character
 	"intl-common:confirm:no": "لا", // no response to a question (in Title Case)
-	"intl-common:confirm:ok": "موافق", // okay response to a question
+	"intl-common:confirm:ok": "حسنًا", // okay response to a question
 	"intl-common:confirm:yes": "نعم", // yes response to a question (in Title Case)
-	"intl-common:navigation:back:title": "الخلف", // term for navigating back to the previous page (in Title Case)
+	"intl-common:navigation:back:title": "العودة", // term for navigating back to the previous page (in Title Case)
 	"intl-common:navigation:continue:title": "متابعة", // continue action (in Title Case)
 	"intl-common:navigation:next:title": "التالي", // term for navigating to the next page or item (in Title Case)
 	"intl-common:navigation:previous:title": "السابق", // term for navigating to the previous page or item (in Title Case)
