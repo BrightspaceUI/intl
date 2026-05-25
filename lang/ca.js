41e39a556e65593f839be18456070e7d4a83b634
tie.d2l.dev <TieUser@d2l.com>
2026-05-25 13:31:48 +0000

    Translation Updates

--- lang/ca.js
+++ lang/ca.js
@@ -1,44 +1,44 @@
 export default {
-	"intl-common:actions:add": "Add", // add action (in Title Case)
-	"intl-common:actions:apply": "Apply", // apply action (in Title Case)
-	"intl-common:actions:browse": "Browse", // browse action (in Title Case)
-	"intl-common:actions:cancel": "Cancel", // cancel action (in Title Case)
-	"intl-common:actions:clear": "Clear", // clear action (in Title Case)
-	"intl-common:actions:close": "Close", // close action (in Title Case)
-	"intl-common:actions:confirm": "Confirm", // confirm action (in Title Case)
-	"intl-common:actions:copy": "Copy", // copy action (in Title Case)
-	"intl-common:actions:create": "Create", // create action (in Title Case)
-	"intl-common:actions:delete": "Delete", // delete action (in Title Case)
-	"intl-common:actions:done": "Done", // done action (in Title Case)
-	"intl-common:actions:edit": "Edit", // edit action (in Title Case)
-	"intl-common:actions:finish": "Finish", // finish action (in Title Case)
-	"intl-common:actions:more": "More", // more action (in Title Case)
-	"intl-common:actions:print": "Print", // print action (in Title Case)
-	"intl-common:actions:remove": "Remove", // remove action (in Title Case)
-	"intl-common:actions:save": "Save", // save action (in Title Case)
-	"intl-common:actions:saveAndClose": "Save and Close", // save and close action (in Title Case)
-	"intl-common:actions:saveAndNew": "Save and New", // save and new action (in Title Case)
-	"intl-common:actions:search": "Search", // search action (in Title Case)
-	"intl-common:actions:submit": "Submit", // submit action (in Title Case)
-	"intl-common:actions:update": "Update", // update action (in Title Case)
-	"intl-common:characters:ampersand": "et", // short name or description of the "&" character
-	"intl-common:characters:apostrophe": "apòstrof tipogràfic", // short name or description of the "'" character
+	"intl-common:actions:add": "Afegir", // add action (in Title Case)
+	"intl-common:actions:apply": "Aplicar", // apply action (in Title Case)
+	"intl-common:actions:browse": "Navega", // browse action (in Title Case)
+	"intl-common:actions:cancel": "Cancel·lar", // cancel action (in Title Case)
+	"intl-common:actions:clear": "Esborrar", // clear action (in Title Case)
+	"intl-common:actions:close": "Tancar", // close action (in Title Case)
+	"intl-common:actions:confirm": "Confirma", // confirm action (in Title Case)
+	"intl-common:actions:copy": "Copiar", // copy action (in Title Case)
+	"intl-common:actions:create": "Crear", // create action (in Title Case)
+	"intl-common:actions:delete": "Eliminar", // delete action (in Title Case)
+	"intl-common:actions:done": "Fet", // done action (in Title Case)
+	"intl-common:actions:edit": "Editar", // edit action (in Title Case)
+	"intl-common:actions:finish": "Finalitzar", // finish action (in Title Case)
+	"intl-common:actions:more": "Més", // more action (in Title Case)
+	"intl-common:actions:print": "Imprimir", // print action (in Title Case)
+	"intl-common:actions:remove": "Eliminar", // remove action (in Title Case)
+	"intl-common:actions:save": "Desar", // save action (in Title Case)
+	"intl-common:actions:saveAndClose": "Desar i tancar", // save and close action (in Title Case)
+	"intl-common:actions:saveAndNew": "Desar i nou", // save and new action (in Title Case)
+	"intl-common:actions:search": "Cerca", // search action (in Title Case)
+	"intl-common:actions:submit": "Lliurar", // submit action (in Title Case)
+	"intl-common:actions:update": "Actualitzar", // update action (in Title Case)
+	"intl-common:characters:ampersand": "signe \"&\"", // short name or description of the "&" character
+	"intl-common:characters:apostrophe": "apòstrof", // short name or description of the "'" character
 	"intl-common:characters:asterisk": "asterisc", // short name or description of the "*" character
-	"intl-common:characters:backslash": "barra inversa", // short name or description of the "\" character
+	"intl-common:characters:backslash": "barra invertida", // short name or description of the "\" character
 	"intl-common:characters:colon": "dos punts", // short name or description of the ":" character
 	"intl-common:characters:comma": "coma", // short name or description of the "," character
-	"intl-common:characters:greaterThan": "major que", // short name or description of the ">" character
-	"intl-common:characters:lessThan": "menor que", // short name or description of the "<" character
-	"intl-common:characters:numberSign": "coixinet", // short name or description of the "#" character
-	"intl-common:characters:percentSign": "tant per cent", // short name or description of the "%" character
+	"intl-common:characters:greaterThan": "signe \"més gran que\"", // short name or description of the ">" character
+	"intl-common:characters:lessThan": "signe de menor que", // short name or description of the "<" character
+	"intl-common:characters:numberSign": "signe numèric", // short name or description of the "#" character
+	"intl-common:characters:percentSign": "signe de percentatge", // short name or description of the "%" character
 	"intl-common:characters:pipe": "barra vertical", // short name or description of the "|" character
-	"intl-common:characters:questionMark": "signe d’interrogació", // short name or description of the "?" character
-	"intl-common:characters:quotationMark": "cometa doble", // short name or description of the '"' character
+	"intl-common:characters:questionMark": "signe d'interrogació", // short name or description of the "?" character
+	"intl-common:characters:quotationMark": "cometes", // short name or description of the '"' character
 	"intl-common:confirm:no": "No", // no response to a question (in Title Case)
-	"intl-common:confirm:ok": "OK", // okay response to a question
-	"intl-common:confirm:yes": "Yes", // yes response to a question (in Title Case)
-	"intl-common:navigation:back:title": "Back", // term for navigating back to the previous page (in Title Case)
-	"intl-common:navigation:continue:title": "Continue", // continue action (in Title Case)
-	"intl-common:navigation:next:title": "Next", // term for navigating to the next page or item (in Title Case)
-	"intl-common:navigation:previous:title": "Previous", // term for navigating to the previous page or item (in Title Case)
+	"intl-common:confirm:ok": "D'acord", // okay response to a question
+	"intl-common:confirm:yes": "Sí", // yes response to a question (in Title Case)
+	"intl-common:navigation:back:title": "Enrere", // term for navigating back to the previous page (in Title Case)
+	"intl-common:navigation:continue:title": "Continua", // continue action (in Title Case)
+	"intl-common:navigation:next:title": "Següent", // term for navigating to the next page or item (in Title Case)
+	"intl-common:navigation:previous:title": "Anterior", // term for navigating to the previous page or item (in Title Case)
 };
