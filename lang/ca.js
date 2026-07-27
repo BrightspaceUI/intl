f51016c5046ba5538cd0896f0a510877b5792a53
tie.d2l.dev <TieUser@d2l.com>
2026-07-27 13:01:18 +0000

    Translation Updates

--- lang/ca.js
+++ lang/ca.js
@@ -1,11 +1,11 @@
 export default {
 	"intl-common:actions:add": "Afegir", // add action (in Title Case)
 	"intl-common:actions:apply": "Aplicar", // apply action (in Title Case)
-	"intl-common:actions:browse": "Navega", // browse action (in Title Case)
+	"intl-common:actions:browse": "Navegar", // browse action (in Title Case)
 	"intl-common:actions:cancel": "Cancel·lar", // cancel action (in Title Case)
 	"intl-common:actions:clear": "Esborrar", // clear action (in Title Case)
 	"intl-common:actions:close": "Tancar", // close action (in Title Case)
-	"intl-common:actions:confirm": "Confirma", // confirm action (in Title Case)
+	"intl-common:actions:confirm": "Confirmar", // confirm action (in Title Case)
 	"intl-common:actions:copy": "Copiar", // copy action (in Title Case)
 	"intl-common:actions:create": "Crear", // create action (in Title Case)
 	"intl-common:actions:delete": "Eliminar", // delete action (in Title Case)
@@ -19,27 +19,27 @@ export default {
 	"intl-common:actions:save": "Desar", // save action (in Title Case)
 	"intl-common:actions:saveAndClose": "Desar i tancar", // save and close action (in Title Case)
 	"intl-common:actions:saveAndNew": "Desar i nou", // save and new action (in Title Case)
-	"intl-common:actions:search": "Cerca", // search action (in Title Case)
+	"intl-common:actions:search": "Cercar", // search action (in Title Case)
 	"intl-common:actions:submit": "Lliurar", // submit action (in Title Case)
 	"intl-common:actions:update": "Actualitzar", // update action (in Title Case)
-	"intl-common:characters:ampersand": "signe «&»", // short name or description of the "&" character
+	"intl-common:characters:ampersand": "signe \"&\"", // short name or description of the "&" character
 	"intl-common:characters:apostrophe": "apòstrof", // short name or description of the "'" character
 	"intl-common:characters:asterisk": "asterisc", // short name or description of the "*" character
 	"intl-common:characters:backslash": "barra invertida", // short name or description of the "\" character
 	"intl-common:characters:colon": "dos punts", // short name or description of the ":" character
 	"intl-common:characters:comma": "coma", // short name or description of the "," character
-	"intl-common:characters:greaterThan": "signe «més gran que»", // short name or description of the ">" character
+	"intl-common:characters:greaterThan": "signe \"més gran que\"", // short name or description of the ">" character
 	"intl-common:characters:lessThan": "signe de menor que", // short name or description of the "<" character
 	"intl-common:characters:numberSign": "signe numèric", // short name or description of the "#" character
 	"intl-common:characters:percentSign": "signe de percentatge", // short name or description of the "%" character
 	"intl-common:characters:pipe": "barra vertical", // short name or description of the "|" character
-	"intl-common:characters:questionMark": "signe d’interrogació", // short name or description of the "?" character
+	"intl-common:characters:questionMark": "signe d'interrogació", // short name or description of the "?" character
 	"intl-common:characters:quotationMark": "cometes", // short name or description of the '"' character
 	"intl-common:confirm:no": "No", // no response to a question (in Title Case)
-	"intl-common:confirm:ok": "D’acord", // okay response to a question
+	"intl-common:confirm:ok": "D'acord", // okay response to a question
 	"intl-common:confirm:yes": "Sí", // yes response to a question (in Title Case)
 	"intl-common:navigation:back:title": "Enrere", // term for navigating back to the previous page (in Title Case)
-	"intl-common:navigation:continue:title": "Continua", // continue action (in Title Case)
+	"intl-common:navigation:continue:title": "Continuar", // continue action (in Title Case)
 	"intl-common:navigation:next:title": "Següent", // term for navigating to the next page or item (in Title Case)
 	"intl-common:navigation:previous:title": "Anterior", // term for navigating to the previous page or item (in Title Case)
 };
