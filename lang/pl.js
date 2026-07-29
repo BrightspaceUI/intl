c42b453a0b0bda4f29e96f7c9dd797ddf78b8acb
tie.d2l.dev <TieUser@d2l.com>
2026-07-29 14:54:22 +0000

    Translation Updates

---
+++ lang/pl.js
@@ -0,0 +1,45 @@
+export default {
+	"intl-common:actions:add": "Dodaj", // add action (in Title Case)
+	"intl-common:actions:apply": "Aplikuj", // apply action (in Title Case)
+	"intl-common:actions:browse": "Przeglądaj", // browse action (in Title Case)
+	"intl-common:actions:cancel": "Anuluj", // cancel action (in Title Case)
+	"intl-common:actions:clear": "Wyczyść", // clear action (in Title Case)
+	"intl-common:actions:close": "Zamknij", // close action (in Title Case)
+	"intl-common:actions:confirm": "Potwierdź", // confirm action (in Title Case)
+	"intl-common:actions:copy": "Kopia", // copy action (in Title Case)
+	"intl-common:actions:create": "Utwórz", // create action (in Title Case)
+	"intl-common:actions:delete": "Usuń", // delete action (in Title Case)
+	"intl-common:actions:done": "Wykonane", // done action (in Title Case)
+	"intl-common:actions:edit": "Edytuj", // edit action (in Title Case)
+	"intl-common:actions:finish": "Zakończenie", // finish action (in Title Case)
+	"intl-common:actions:more": "Więcej", // more action (in Title Case)
+	"intl-common:actions:print": "Drukuj", // print action (in Title Case)
+	"intl-common:actions:reload": "Reload", // reload action (in Title Case)
+	"intl-common:actions:remove": "Usuń", // remove action (in Title Case)
+	"intl-common:actions:save": "Zapisz", // save action (in Title Case)
+	"intl-common:actions:saveAndClose": "Zapisz i zamknij", // save and close action (in Title Case)
+	"intl-common:actions:saveAndNew": "Zapisz i utwórz nowy", // save and new action (in Title Case)
+	"intl-common:actions:search": "Wyszukiwanie", // search action (in Title Case)
+	"intl-common:actions:submit": "Prześlij", // submit action (in Title Case)
+	"intl-common:actions:update": "Aktualizuj", // update action (in Title Case)
+	"intl-common:characters:ampersand": "ampersand", // short name or description of the "&" character
+	"intl-common:characters:apostrophe": "apostrof", // short name or description of the "'" character
+	"intl-common:characters:asterisk": "gwiazdka", // short name or description of the "*" character
+	"intl-common:characters:backslash": "ukośnik wsteczny", // short name or description of the "\" character
+	"intl-common:characters:colon": "dwukropek", // short name or description of the ":" character
+	"intl-common:characters:comma": "przecinek", // short name or description of the "," character
+	"intl-common:characters:greaterThan": "znak większości", // short name or description of the ">" character
+	"intl-common:characters:lessThan": "znak mniejszości", // short name or description of the "<" character
+	"intl-common:characters:numberSign": "znak numeryczny", // short name or description of the "#" character
+	"intl-common:characters:percentSign": "znak procentowy", // short name or description of the "%" character
+	"intl-common:characters:pipe": "pionowa kreska", // short name or description of the "|" character
+	"intl-common:characters:questionMark": "znak zapytania", // short name or description of the "?" character
+	"intl-common:characters:quotationMark": "cudzysłów", // short name or description of the '"' character
+	"intl-common:confirm:no": "Nie", // no response to a question (in Title Case)
+	"intl-common:confirm:ok": "OK", // okay response to a question
+	"intl-common:confirm:yes": "Tak", // yes response to a question (in Title Case)
+	"intl-common:navigation:back:title": "Wstecz", // term for navigating back to the previous page (in Title Case)
+	"intl-common:navigation:continue:title": "Kontynuuj", // continue action (in Title Case)
+	"intl-common:navigation:next:title": "Dalej", // term for navigating to the next page or item (in Title Case)
+	"intl-common:navigation:previous:title": "Poprzedni", // term for navigating to the previous page or item (in Title Case)
+};
---
+++ lang/ru.js
@@ -0,0 +1,45 @@
+export default {
+	"intl-common:actions:add": "Добавить", // add action (in Title Case)
+	"intl-common:actions:apply": "Применить", // apply action (in Title Case)
+	"intl-common:actions:browse": "Просматривать", // browse action (in Title Case)
+	"intl-common:actions:cancel": "Отменить", // cancel action (in Title Case)
+	"intl-common:actions:clear": "Очистить", // clear action (in Title Case)
+	"intl-common:actions:close": "Закрыть", // close action (in Title Case)
+	"intl-common:actions:confirm": "Подтвердите", // confirm action (in Title Case)
+	"intl-common:actions:copy": "Копия", // copy action (in Title Case)
+	"intl-common:actions:create": "Создать", // create action (in Title Case)
+	"intl-common:actions:delete": "Удалить", // delete action (in Title Case)
+	"intl-common:actions:done": "Выполнено", // done action (in Title Case)
+	"intl-common:actions:edit": "Редактировать", // edit action (in Title Case)
+	"intl-common:actions:finish": "Готово", // finish action (in Title Case)
+	"intl-common:actions:more": "Подробнее", // more action (in Title Case)
+	"intl-common:actions:print": "Распечатать", // print action (in Title Case)
+	"intl-common:actions:reload": "Reload", // reload action (in Title Case)
+	"intl-common:actions:remove": "Удалить", // remove action (in Title Case)
+	"intl-common:actions:save": "Сохранить", // save action (in Title Case)
+	"intl-common:actions:saveAndClose": "Сохранить и закрыть", // save and close action (in Title Case)
+	"intl-common:actions:saveAndNew": "Сохранить и создать", // save and new action (in Title Case)
+	"intl-common:actions:search": "Поиск", // search action (in Title Case)
+	"intl-common:actions:submit": "Отправить", // submit action (in Title Case)
+	"intl-common:actions:update": "Обновить", // update action (in Title Case)
+	"intl-common:characters:ampersand": "Амперсанд", // short name or description of the "&" character
+	"intl-common:characters:apostrophe": "Апостроф", // short name or description of the "'" character
+	"intl-common:characters:asterisk": "Звездочка", // short name or description of the "*" character
+	"intl-common:characters:backslash": "Обратная косая черта", // short name or description of the "\" character
+	"intl-common:characters:colon": "Двоеточие", // short name or description of the ":" character
+	"intl-common:characters:comma": "запятая", // short name or description of the "," character
+	"intl-common:characters:greaterThan": "Знак «больше»", // short name or description of the ">" character
+	"intl-common:characters:lessThan": "знак «меньше»", // short name or description of the "<" character
+	"intl-common:characters:numberSign": "символ номера", // short name or description of the "#" character
+	"intl-common:characters:percentSign": "знак процента", // short name or description of the "%" character
+	"intl-common:characters:pipe": "труба", // short name or description of the "|" character
+	"intl-common:characters:questionMark": "Вопросительный знак", // short name or description of the "?" character
+	"intl-common:characters:quotationMark": "кавычка", // short name or description of the '"' character
+	"intl-common:confirm:no": "Нет", // no response to a question (in Title Case)
+	"intl-common:confirm:ok": "OK", // okay response to a question
+	"intl-common:confirm:yes": "Да", // yes response to a question (in Title Case)
+	"intl-common:navigation:back:title": "Назад", // term for navigating back to the previous page (in Title Case)
+	"intl-common:navigation:continue:title": "Продолжить", // continue action (in Title Case)
+	"intl-common:navigation:next:title": "Далее", // term for navigating to the next page or item (in Title Case)
+	"intl-common:navigation:previous:title": "Предыдущая", // term for navigating to the previous page or item (in Title Case)
+};
